9438fd96460ee7a5c44412a43cd3f71203c21aca
etolizaa <153313507+etolizaa@users.noreply.github.com>
2023-12-19 16:09:06 +0300

    Update register_controller.php

--- tickets/authentication/register_controller.php
+++ tickets/authentication/register_controller.php
@@ -7,11 +7,6 @@ $login = trim($_POST['login'] ?? '');
 $password = trim($_POST['password'] ?? '');
 $confirmPassword = trim($_POST['confirmPassword'] ?? '');
 
-if (empty($login) || empty($password) || empty($confirmPassword) || !preg_match('/^[a-zA-Z0-9]+$/', $login) || !preg_match('/^[a-zA-Z0-9]+$/', $password)) {
-    echo json_encode(['success' => false, 'error' => 'Неверно заполены поля.']);
-    exit;
-}
-
 if ($password !== $confirmPassword) {
     echo json_encode(['success' => false, 'error' => 'Пароли не совпадают.']);
     exit;
